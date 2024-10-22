c3681bc9485c778eb3aaf041843fbeb2f1e17b7e
german nunez <german90nunez@gmail.com>
2024-10-22 16:30:13 -0700

    restructure folders, mobile navbar v1

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import styles from './navbar.module.css'
 
 
 const navLinks: string[] = ["Home", "About", "Blogs", "Contact"]
 const Navbar = () => {
+  const [ isMobile, setIsMobile ] = useState<boolean> (false)
   return (
     <nav className={styles.navbar}>
         <div className={styles.logo}>THE BIG WAG</div>
@@ -14,6 +16,27 @@ const Navbar = () => {
                 ))
             }
         </div>
+
+        <div className={styles.hamburgerMenu} onClick={() => setIsMobile(!isMobile)}>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+        </div>
+        
+        {
+          isMobile 
+          ?
+            <div className={styles.mobileMenu}>
+              {
+                  navLinks.map((link: string) => (
+                      <a href="#" onClick={() => setIsMobile(false)} key={link} className={styles.navlink}>{link}</a>
+                  ))
+              }
+            </div>
+          :
+            <></>
+        }
+
     </nav>
   )
 }
